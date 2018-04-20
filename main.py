d5b140cd726f619a547c3eb3c41210f5cccc78e6
Alex Kim <disy455@gmail.com>
2018-04-20 11:41:20 -0400

    Added opus_loader.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import discord
 import os
-import ctypes.util
+from util.opus_loader import load_opus_lib
 from discord.ext import commands
 
 Client = discord.Client()
+load_opus_lib()
 bot_prefix = "!"
 bot = commands.Bot(command_prefix=bot_prefix)
 

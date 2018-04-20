be03164fa11f23ad2a3d53ac5bf829d685d52cf0
Alex Kim <disy455@gmail.com>
2018-04-19 20:55:21 -0400

    Updated Wiki Command

--- cmd/wiki.py
+++ cmd/wiki.py
@@ -1,5 +1,14 @@
 import discord
+import wikipedia
 from discord.ext import commands
+
+
+def replace_char(string):
+    s = list(string)
+    for i in range(0, len(s)):
+        if s[i] == " ":
+            s[i] = "_"
+    return "".join(s)
 
 
 class Wiki:
@@ -12,15 +21,33 @@ class Wiki:
         string.pop(0)
         if len(string) == 0:
             error_embed = discord.Embed(title="Command Help",
-                                        description="!wiki word",
+                                        description="!wiki word\n"
+                                                    "This command only works in English at the moment!",
                                         colour=discord.Colour.purple())
             await self.bot.say(embed=error_embed)
         else:
             message = ""
             for i in range(0, len(string)):
-                message += string[0]
-            embed = discord.Embed(url="https://en.wikipedia.org/wiki/"+message)
-            await self.bot.say(embed=embed)
+                message += string[i] + " "
+            try:
+                wiki_page = wikipedia.page(title=message, auto_suggest=True, redirect=True)
+                title = wiki_page.title
+                url_title = replace_char(title)
+                url = "https://en.wikipedia.org/wiki/"+url_title
+                description = wikipedia.summary(title, chars=2000)
+                embed = discord.Embed(title=title,
+                                      description=description,
+                                      url=url,
+                                      colour=discord.Colour.blue())
+                await self.bot.say(embed=embed)
+            except wikipedia.DisambiguationError as e:
+                string = ""
+                for i in range(0, 20):
+                    string += "* " + e.options[i] + '\n'
+                exception_embed = discord.Embed(title="Disambiguation. Try to be more specific!",
+                                                description=string,
+                                                colour=discord.Colour.red())
+                await self.bot.say(embed=exception_embed)
 
 
 def setup(bot):

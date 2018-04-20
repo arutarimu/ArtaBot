d6a141c61166ff34b937db582e3e2f131ee0d87e
Alex Kim <disy455@gmail.com>
2018-04-20 10:49:39 -0400

    Heroku

--- cmd/music.py
+++ cmd/music.py
@@ -1,10 +1,11 @@
 import asyncio
 import discord
 from discord.ext import commands
+import ctypes.util
 from util import exception_handler
 
 if not discord.opus.is_loaded():
-    discord.opus.load_opus('opus')
+    ctypes.util.find_library("libopus")
 
 
 class VoiceEntry:
